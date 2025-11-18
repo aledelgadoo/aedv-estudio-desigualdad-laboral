8a9ab0cbc408de84bef8e3fa7b2d34d1ddcd8f6b
aledelgadoo <alejandrodelgadovalera@gmail.com>
2025-11-18 15:53:00 +0000

    Añadidos archivos geoj en local para una lectura más rápida.

--- scripts/guardar_datasets_local.R
+++ scripts/guardar_datasets_local.R
@@ -2,6 +2,7 @@ library(readr)
 library(dplyr)
 library(openxlsx)
 library(eurostat)
+library(geojsonio)
 
 dataset.name <- "nama_10r_2lp10"
 
@@ -26,3 +27,13 @@ datasets <- read.xlsx("https://ctim.ulpgc.es/AEDV/data/Eurostat.datasets.xlsx") 
 
 # Guardar la fila filtrada como CSV local
 write_csv(datasets, paste0("data/processed/", dataset.name, "_info.csv"))
+
+## ARCHIVOS GEOJ ##
+geoj0 <- geojson_read("https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_20M_2024_4326_LEVL_0.geojson",  what = "sp")
+geoj1 <- geojson_read("https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_20M_2024_4326_LEVL_1.geojson",  what = "sp")
+geoj2 <- geojson_read("https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_20M_2024_4326_LEVL_2.geojson",  what = "sp")
+
+# Guardamos los objetos Espaciales (mapas)
+saveRDS(geoj0, "data/geoj0.rds")
+saveRDS(geoj1, "data/geoj1.rds")
+saveRDS(geoj2, "data/geoj2.rds")
